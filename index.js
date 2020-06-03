5f11f210465b8966a1c1f43d4cb48b7f8d16d8db
Yangfan Zhang <yf_chang@icloud.com>
2020-06-03 21:52:18 +1000

    Dynamically render array items in bootstrap alerts.

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-var AllMembers = ["Rachel", "Ross", "Phoebe", "Joey", "Chandler", "Monica"];
-var MembersRoundOne = [ "Ross", "Phoebe"];
+var AllMembers = ["Rachel Green", "Ross", "Ross", "Phoebe", "Joey Tribianni", "Chandler Bing", "Monica Gellar"];
+var PastMembers = [ "Ross", "Phoebe"];
+
+AllMembers.forEach(element => {
+    $(".AllList").append("<div class=\"alert alert-warning\" role=\"alert\"><strong>"+ element +"</strong></div>"); 
+});
+
+PastMembers.forEach(element => {
+    $(".PastList").append("<div class=\"alert alert-warning\" role=\"alert\"><strong>" + element +
+                            "  <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button></strong></div>");
+});
+
+
+
+
+
+
 
 $('.modal-body').text(NextPicker());
 
 function NextPicker(){
-    var membersToChoose = $(AllMembers).not(MembersRoundOne).get();
+    var membersToChoose = $(AllMembers).not(PastMembers).get();
     var randomNumber = Math.floor(Math.random()*membersToChoose.length);
     return membersToChoose[randomNumber];
 }
