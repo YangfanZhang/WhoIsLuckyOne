e707b094a29c8c90b215636f5a2b52e8dd34fd21
Yangfan Zhang <yf_chang@icloud.com>
2020-06-05 00:08:28 +1000

    Added features to 'roll again', 'delete past pickers' and 'save'.

--- index.js
+++ index.js
@@ -1,24 +1,63 @@
-var AllMembers = ["Adam", "Chris", "Eric", "Francois", "George", "Harry", "Hening", "James J", "James M", "Jayne", "Jia", "Johan", "Karthik", "Leighton", "Mark G", "Mark L", "Marty", "Matt", "Mike", "Pat", "Phillip", "Reesy", "Romey", "Shane", "Tyson", "Wesley", "Yangfan", "Yoong"];
-AllMembers.sort();
-var RoundOne = [ "Marty", "Yoong", "Reesy", "James J", "Matt", "James M", "Leighton", "Yangfan"];
-RoundOne.sort();
-AppendItemsInAllMembers(AllMembers);
-AppendItemsInRoundOne(RoundOne);
+let allMembers = ["Adam", "Chris", "Eric", "Francois", "George", "Harry", "Hening", "James J", "James M", "Jayne", "Jia", "Johan", "Karthik", "Leighton", "Mark G", "Mark L", "Marty", "Matt", "Mike", "Pat", "Phillip", "Reesy", "Romey", "Shane", "Tyson", "Wesley", "Yangfan", "Yoong"];
+allMembers.sort();
+AppendItemsInAllMembers(allMembers);
 
-$('.modal-body').text(NextPicker());
+
+if(localStorage.getItem('roundOneLocal') == null){
+    roundOne = [ "Marty", "Yoong", "Reesy", "James J", "Matt", "James M", "Leighton", "Yangfan"];
+    localStorage.setItem("roundOneLocal", JSON.stringify(roundOne));
+}
+else
+{
+    roundOne =  JSON.parse(localStorage.getItem('roundOneLocal'));
+}
+roundOne.sort();
+AppendItemsInroundOne(roundOne);
+
+
+$("#generateBtn").on('click', function(e){
+    rollAgain(e);
+});
+
+$('.rollAgain').on('click', function(e){
+    rollAgain(e);
+});
+
+$('.saveBtn').on('click', function(e){
+    var textValue = $('.modal-body').text();
+    roundOne =  JSON.parse(localStorage.getItem('roundOneLocal'));
+    roundOne.push(textValue);
+    localStorage.setItem("roundOneLocal", JSON.stringify(roundOne));
+    var col = selectCol(roundOne.length-1);
+    $(".PastList" + col).append("<div class=\"alert alert-secondary \" role=\"alert\"><strong>" + textValue +
+    "<button type=\"button\" class=\"close closeAlertBtn\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button></strong></div>");
+})
+
+$('.closeAlertBtn').on('click', function(e){
+    var alert = e.currentTarget.parentNode;
+    var text = alert.textContent.replace('×', '');
+    roundOne =  JSON.parse(localStorage.getItem('roundOneLocal'));
+    roundOne.splice(roundOne.indexOf(text), 1);
+    localStorage.setItem("roundOneLocal", JSON.stringify(roundOne));
+})
+
+function rollAgain(event){
+    event.preventDefault();
+    $('.modal-body').text(NextPicker());
+}
 
 function AppendItemsInAllMembers(array) {
     for (let i = 0; i < array.length; i++) {
         var col = selectCol(i);
         $(".AllList" + col).append("<div class=\"alert alert-secondary \" role=\"alert\"><strong>"+ array[i] +"</strong></div>");
-    }
+        }
 }
 
-function AppendItemsInRoundOne(array){
+function AppendItemsInroundOne(array){
     for (let i = 0; i < array.length; i++) {
         var col = selectCol(i);
         $(".PastList" + col).append("<div class=\"alert alert-secondary \" role=\"alert\"><strong>" + array[i] +
-        "  <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button></strong></div>");
+        "<button type=\"button\" class=\"close closeAlertBtn\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button></strong></div>");
     }
 }
 
@@ -37,7 +76,7 @@ function selectCol(i) {
 }
 
 function NextPicker(){
-    var membersToChoose = $(AllMembers).not(RoundOne).get();
+    var membersToChoose = $(allMembers).not(roundOne).get();
     var randomNumber = Math.floor(Math.random()*membersToChoose.length);
     return membersToChoose[randomNumber];
 }
